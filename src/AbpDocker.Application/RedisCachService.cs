c23353bc7d20faec6825149cbc986ca3b02d0e46
Mohammad Mahafdah <mmahafdeh@thiqah.sa>
2022-01-30 15:14:25 +0300

    add redis cach to docker compose and connect it to projects

---
+++ src/AbpDocker.Application/RedisCachService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Caching;
+using Volo.Abp.DependencyInjection;
+
+namespace AbpDocker
+{
+    public class RedisCachService : ApplicationService, ITransientDependency
+    {
+        private readonly IDistributedCache<string> _cache;
+
+        public RedisCachService(IDistributedCache<string> cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<string> OnGetAsync(string key)
+        {
+            return await _cache.GetAsync(key);
+        }
+
+        public async Task<string> OnPostAsync(string key,string value)
+        {
+            //var options = new DistributedCacheEntryOptions()
+            //    .SetSlidingExpiration(TimeSpan.FromSeconds(20));
+            await _cache.SetAsync(key, value);
+
+            return key;
+        }
+    }
+}
